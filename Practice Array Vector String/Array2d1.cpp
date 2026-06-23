00ee34957816d4dbdafa2056d461b39d00d4a78d
Zibozzz <j.dumsuk@gmail.com>
2026-06-23 23:29:51 +0700

    Add 2D array input and output functionality
    Implement a 2D array input and output in C++.

---
+++ Practice Array Vector String/Array2d1.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[1005][1005];
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> a[i][j];
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cout << a[i][j] << ' ';
+        }
+        cout << "\n";
+    }
+}
+
+/*
+3
+1 2 3
+4 5 6
+7 8 9
+*/
